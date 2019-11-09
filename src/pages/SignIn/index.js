8ff9f19e399add5becbf1b86adb2aa26faba5ea8
= <nncl@live.com>
2019-11-09 17:42:46 -0300

    Styling forms

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>Sign In</h1>;
+  return (
+    <>
+      <img src={logo} alt="Go Barber" />
+
+      <form action="">
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Password" />
+
+        <button type="submit">Sign in</button>
+        <Link to="/register">Create free account</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="Go Barber" />
+
+      <form action="">
+        <input type="text" placeholder="Fullname" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Password" />
+
+        <button type="submit">Create</button>
+        <Link to="/">Already have an account?</Link>
+      </form>
+    </>
+  );
 }
