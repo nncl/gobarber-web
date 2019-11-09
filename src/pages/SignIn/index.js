6d81e01970025b3fdd91ad331bcbcf4260621f76
= <nncl@live.com>
2019-11-09 18:07:58 -0300

    Setting up unform from rocketseat package

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Go Barber" />
 
-      <form action="">
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
 
         <button type="submit">Sign in</button>
         <Link to="/register">Create free account</Link>
-      </form>
+      </Form>
     </>
   );
 }
