5af7b895dca507b7681074516922fc45d9a90149
= <nncl@live.com>
2019-11-09 21:36:57 -0300

    Adding authentication through redux

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,28 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import api from '~/services/api';
+import { signInSuccess } from './actions';
+import history from '~/services/history';
 
-export default all([]);
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'auth', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.log(`User is not a provider`);
+    return;
+  }
+
+  yield put(signInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+]);
