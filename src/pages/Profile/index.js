36c04ec49f29452c7a3d23bc9c53bbe0c8b42169
= <nncl@live.com>
2019-11-10 22:53:08 -0300

    Styling profile page and getting data from reducer

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit() {
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="E-mail" />
+
+        <hr />
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Current password"
+        />
+
+        <Input name="password" type="password" placeholder="New password" />
+
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm password"
+        />
+
+        <button type="submit">Update Profile</button>
+      </Form>
+
+      <button type="button">Log out</button>
+    </Container>
+  );
 }
