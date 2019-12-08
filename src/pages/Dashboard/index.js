fb5bf54b81038d52f0c108853430fe072364223d
= <nncl@live.com>
2019-12-07 23:50:42 -0300

    Dashboard styling

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>May 31st</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Caue Almeida</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Caue Almeida</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Caue Almeida</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
