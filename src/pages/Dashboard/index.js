84599140693bba4a62038be512a942bf7d359132
= <nncl@live.com>
2019-12-08 01:34:08 -0300

    Listing schedules

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,58 @@
-import React, { useState, useMemo } from 'react';
-import { format, subDays, addDays } from 'date-fns';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  setMilliseconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import { Container, Time } from './styles';
+import api from '~/services/api';
+
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 export default function Dashboard() {
+  const [schedule, setSchedule] = useState([]);
   const [date, setDate] = useState(new Date());
   const formattedDate = useMemo(() => format(date, 'MMMM do'), [date]);
+
+  useEffect(() => {
+    async function loadSchedules() {
+      const response = await api.get(`schedules`, {
+        params: { date },
+      });
+
+      // Get timezone from user's browser
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const checkDate = setMilliseconds(
+          setSeconds(setMinutes(setHours(date, hour), 0), 0),
+          0,
+        );
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(a =>
+            isEqual(parseISO(a.date), compareDate),
+          ),
+        };
+      });
+
+      setSchedule(data);
+    }
+
+    loadSchedules();
+  }, [date]);
 
   function handlePrevDay() {
     setDate(subDays(date, 1));
@@ -28,22 +75,14 @@ export default function Dashboard() {
       </header>
 
       <ul>
-        <Time past>
-          <strong>08:00</strong>
-          <span>Caue Almeida</span>
-        </Time>
-        <Time available>
-          <strong>09:00</strong>
-          <span>Em aberto</span>
-        </Time>
-        <Time>
-          <strong>10:00</strong>
-          <span>Caue Almeida</span>
-        </Time>
-        <Time>
-          <strong>11:00</strong>
-          <span>Caue Almeida</span>
-        </Time>
+        {schedule.map(time => (
+          <Time past={time.past} available={!time.appointment} key={time.time}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : 'Available'}
+            </span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   max-width: 600px;
-  margin: 50px auto;
+  margin: 50px auto 0;
   display: flex;
   flex-direction: column;
+  padding-bottom: 15px;
   header {
     display: flex;
     align-self: center;
